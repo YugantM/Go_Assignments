f9fb180206b384cec1362a2d92081e3f37478f67
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2019-07-15 20:38:26 +0530

    Update SingleLinkedList.go

--- SingleLinkedList.go
+++ SingleLinkedList.go
@@ -9,36 +9,51 @@ type node struct {
 };
 
 func main() {
-    //reading an integer
+    //variables
     var number int
 	var temp int
 
+	//nodes (structures)  
 	var head node
 	var pin *node
 	
+	//getting input from user to define size of the linked list
     fmt.Println("Enter a number for size of the list")
     fmt.Scan(&number)
 
+	fmt.Println("Enter values for the linked list")
+	//assigning temp value from user to head node
 	fmt.Scan(&temp)
 	head.val = temp 
 	
+	//secondary pointer pointing to the head node
 	pin = &head
 	
+	//loop for adding nodes 
 	for index := 1; index < number; index++ {
+		
+		//checking if the loop is on the last number or not
 		if(index!=number){
-		pin.next = new(node)
+		pin.next = new(node)  // if not on last number, allocating new node to the next of the current one
 		}else{
-		pin.next = nil
+		pin.next = nil	      // if yes allocate nil to the next of the last node
 		}
+		
+		//getting values from user and adding it to the current node
 		pin = pin.next
 		fmt.Scan(&temp)
 		pin.val = temp 
-//		fmt.Println(index)
+
 	}
 
-	
+	fmt.Println("Printing the linked list")
+	//loop starts with pin(secondary node) set to the head node, loop prints the value of the node untill the next of the node is pointing to nil
+	fmt.Print(" | ")
 	for pin = &head; pin != nil; pin=pin.next {
-		fmt.Println(pin.val)
+		fmt.Print(pin.val," | ")
+		if (pin.next==nil){
+			fmt.Print("end")
+		}
 	}
 
 }
