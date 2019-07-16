0f252c746f736d1f7c40356df5467d1d7d2f7bd6
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2019-07-16 16:04:34 +0530

    Update launch.go

--- WebApp/launch.go
+++ WebApp/launch.go
@@ -1,13 +1,19 @@
 package main
 
 import ("fmt"
+        "log"
         "net/http")
 
+// the handler function for "/" route/home
 func index_page(response http.ResponseWriter,request *http.Request){
   fmt.Fprintf(response, "Hello there!")
 }
 
 func main(){
+
+  //it routes the given handler function
   http.HandleFunc("/",index_page)
-  http.ListenAndServe(":8000",nil)
+
+  //starting the server on desired port, second argument is for handler which is nil
+  log.Fatal(http.ListenAndServe(":8000",nil))
 }
