1412d7a1ac2d9061b4210c01bd9ec290cf6c6727
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2019-07-16 14:51:08 +0530

    Update SingleLinkedList.go

--- SingleLinkedList.go
+++ SingleLinkedList.go
@@ -10,39 +10,39 @@ type node struct {
 
 func main() {
     //variables
-    var number int
+  var number int
 	var temp int
 
-	//nodes (structures)  
+	//nodes (structures)
 	var head node
 	var pin *node
-	
+
 	//getting input from user to define size of the linked list
-    fmt.Println("Enter a number for size of the list")
-    fmt.Scan(&number)
+  fmt.Println("Enter a number for size of the list")
+  fmt.Scan(&number)
 
 	fmt.Println("Enter values for the linked list")
 	//assigning temp value from user to head node
 	fmt.Scan(&temp)
-	head.val = temp 
-	
+	head.val = temp
+
 	//pointer pointing to the head node
 	pin = &head
-	
-	//loop for adding nodes 
+
+	//loop for adding nodes
 	for index := 1; index < number; index++ {
-		
+
 		//checking if the loop is on the last number or not
 		if(index!=number){
 		pin.next = new(node)  // if not on last number, allocating new node to the next of the current one
 		}else{
 		pin.next = nil	      // if yes allocate nil to the next of the last node
 		}
-		
+
 		//getting values from user and adding it to the current node
 		pin = pin.next
 		fmt.Scan(&temp)
-		pin.val = temp 
+		pin.val = temp
 
 	}
 
