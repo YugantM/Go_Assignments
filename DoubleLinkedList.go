c867a35279d4592821ee26fca512b3d1437900b9
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2019-07-17 14:08:33 +0530

    linked list changes

--- DoubleLinkedList.go
+++ DoubleLinkedList.go
@@ -4,78 +4,54 @@ import "fmt"
 
 
 type node struct {
+   previous *node
    val int
-   next,previous *node
+   next *node
 };
 
 func main() {
-    //variables
-  var number int
-	var temp int
-
-	//nodes (structures)
-	var head,tail node
-	var pin *node
-
+  var val,number int
 	//getting input from user to define size of the linked list
   fmt.Println("Enter a number for size of the list")
   fmt.Scan(&number)
 
 	fmt.Println("Enter values for the linked list")
 	//assigning temp value from user to head node
-	fmt.Scan(&temp)
-	head.val = temp
+	fmt.Scan(&val)
+  head := &node{nil,val,nil}
   head.previous = nil
+  pin := head
+  temp := head
 
-	//pointer pointing to the head node
-	pin = &head
-  fmt.Println("---printing head::---\n)
-	//loop for adding nodes
-	for index := 1; index < number; index++ {
+  for index:=1; index<number;index++{
+    fmt.Scan(&val)
+    pin.next = &node{nil,val,nil}
+    temp = pin
+    pin = pin.next
+    pin.previous = temp
+  }
+    tail := pin
 
-		//checking if the loop is on the last number or not
-		if(index!=number){
-
-    pin.previous = pin
-    tail = *pin
-    pin.next = new(node)
-    // if not on last number, allocating new node to the next of the current one
-
-  	}else{
-		pin.next = nil	      // if yes allocate nil to the next of the last node
-		}
-
-		//getting values from user and adding it to the current node
-		pin = pin.next
-		fmt.Scan(&temp)
-		pin.val = temp
-    fmt.Println("prev:",pin.previous,"\nval",pin.val,"\nnext:",pin.next,"\ntail\n\n:",&tail,"\n\n")
-	}
-
-/*  tail = pin.previous
-
-	fmt.Println("Printing the forward linked list")
+	fmt.Println("\n\n----Printing the forward linked list----\n")
 	//loop starts with pin(pointer) set to the head node, loop prints the value of the node untill the next of the node is pointing to nil
 	fmt.Print(" | ")
-	for pin = &head; pin != nil; pin=pin.next {
+	for pin = head; pin != nil; pin=pin.next {
 		fmt.Print(pin.val," | ")
 		if (pin.next==nil){
 			fmt.Print("end")
 		}
 	}
-*/
 
-  //fmt.Println("\n\nPrinting the backward linked list")
 
-//  fmt.Println("garbage\n\n",tail.val,tail.previous,tail.next)
+  fmt.Println("\n\n----Printing the backward linked list----\n")
 	//loop starts with pin(pointer) set to the head node, loop prints the value of the node untill the next of the node is pointing to nil
-	//fmt.Print(" | ")
-/*	for pin = &tail; pin != nil; pin=pin.previous {
+  fmt.Print(" | ")
+	for pin = tail; pin != nil; pin=pin.previous {
 		fmt.Print(pin.val," | ")
 		if (pin.previous==nil){
 			fmt.Print("start")
 		}
-	}*/
+	}
 
 
 }
