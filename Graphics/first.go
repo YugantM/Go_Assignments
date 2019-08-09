8442f969c8d91efc47c61dc2fbfc937912838029
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2019-08-09 14:54:35 +0530

    updates

--- Graphics/first.go
+++ Graphics/first.go
@@ -3,9 +3,10 @@ package main
 import "github.com/fogleman/gg"
 
 func main() {
-    dc := gg.NewContext(16, 16)
-    dc.DrawCircle(4, 4, 2)
-    dc.SetRGB(250, 5,155)
-    dc.Fill()
+    dc := gg.NewContext(400, 400)
+
+    dc.DrawCircle(40, 40, 20)
+    dc.SetRGB(250,205,155)
+    
     dc.SavePNG("out.png")
 }
